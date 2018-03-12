85886dea09eac48128785b48e5af96d09973d1e2
Kai Evenson <kevenson@bcis.bates.edu>
2018-03-12 13:27:55 -0400

    first commit

---
+++ Assets/PerformanceBounceback/Scripts/Score.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+    public GameManager gameManager;
+	private Text text;
+
+	// Use this for initialization
+	void Start () {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+		text = GetComponentInChildren<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// move this to Start
+       // Text text = GetComponentInChildren<Text>();
+        text.text = "Score: " + gameManager.score.ToString();
+
+		
+	}
+}
